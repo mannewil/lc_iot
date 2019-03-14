f123f3b6332d28409d89a878852234b3cb7c1922
mannewil <36708883+mannewil@users.noreply.github.com>
2019-03-14 20:41:16 +0500

    working programs
    alhgejhsO:GHsOghsr"OighdF"OHIZA{OIHjah'lkjzdfh'lkJDARH[io

---
+++ config.h
@@ -0,0 +1,10 @@
+#define wifi_ssid     "xtramqtt"
+#define wifi_pass     "12345876"
+
+#define mqtt_server     "2.remzalp.ru"
+#define mqtt_port     9976
+#define mqtt_login    "student"
+#define mqtt_pass     "rtf-123"
+#define mqtt_topic_in  "Demchenko_xoxo/tmp"
+#define mqtt_topic_heartbeat  "Demchenko_xoxo/hb"
+#define mqtt_topic_out  "Demchenko_xoxo/out"
